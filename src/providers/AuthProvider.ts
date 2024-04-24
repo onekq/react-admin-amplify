1a6277d40a77f6a1f312bfb67ce67804b8bbf208
Yi Cui <developers@onekq.ai>
2024-04-24 04:18:33 -0700

    decodeJWT

--- src/providers/AuthProvider.ts
+++ src/providers/AuthProvider.ts
@@ -1,5 +1,4 @@
-import { signIn, signOut, fetchAuthSession } from "@aws-amplify/auth";
-import * as jwt from 'jsonwebtoken'; 
+import { signIn, signOut, fetchAuthSession, decodeJWT } from "@aws-amplify/auth";
 
 export interface AuthProviderOptions {
   authGroups?: string[];
@@ -62,8 +61,7 @@ export class AuthProvider {
     const session = await fetchAuthSession();
 
     const token = session.tokens.idToken.toString();
-    const payload = jwt.decode(token) as { [key: string]: any };
-    const groups = payload['cognito:groups'];  
+    const groups = decodeJWT(token)['cognito:groups'];  
 
     return groups ? Promise.resolve(groups) : Promise.reject();
   };
